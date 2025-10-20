b17e6497a29e3dcfdb5f3b9e952fb56d1140506b
EODIEGOTELES <07diegomc07@gmail.com>
2025-10-20 20:51:56 -0300

    Atualizando o codigo

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import { filmes } from '../data/filmes';
+
+function Home () {
+    console.log(filmes);
+
+    const primeiroFilme = filmes[0];
+
+    const estiloImagem = {
+        width: "300px"
+    };
+
+    return (
+        <>
+            <h1>Filmes</h1>
+
+            <ul>
+                {
+                    filmes.map((filme) => {
+                        return (
+                            <li>
+                                <p>Título: {filme.titulo}</p>
+                                <p>Ano: {filme.ano}</p>
+                                <p>Gênero: {filme.genero}</p>
+                                <p>Nota: {filme.nota}</p>
+
+                                <img style={estiloImagem} src={filme.poster}/>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </>
+    );
+}
+
+export default Home;
