3b44c368648bf42f9d11bcce9d7cc86ed76085d0
Diego <07diegomc07@gmail.com>
2025-12-04 15:13:42 -0300

    arquivo completo de serviços web

---
+++ web service/pages/index.js
@@ -0,0 +1,28 @@
+import {filmes} from "../data/filmes.js";
+import CardFilme from '../components/CardFilme';
+
+function Home () {
+    const estiloListaCards = {
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+        gap: '16px',
+        listStyle: 'none',
+    }
+    return <>  
+        <h1>Filmes</h1>
+
+        <ul style = {estiloListaCards}>
+            {
+                filmes.map((filme) => {
+                    return (
+                        <CardFilme key={`card-filme-${filme.id}`} filme={filme}/>
+                    );
+                })
+            }
+            
+        </ul>
+
+        </>
+}
+
+export default Home;
